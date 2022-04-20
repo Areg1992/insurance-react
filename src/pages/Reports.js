93af0bdb6e639906e549e89b01691253e75b2691
Areg Karoyan <karoyan92@gmail.com>
2022-04-20 19:36:08 +0400

    copy to cliip

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,63 +1,75 @@
 import React from "react";
 import './Reports.scss';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import coverImg1 from '../assets/img/coverImg1.png';
 import coverImg2 from '../assets/img/Group_43.png';
 import Card from "../components/Card/Card";
 
-function Reports() {
-    const cardList = [
-        {
-            totalPrice: 1000,
-            policyPrice: 400,
-            claimPrice: 800
-        },
+const cardList = [
+    {
+        totalPrice: 1000,
+        policyPrice: 400,
+        claimPrice: 800
+    },
 
-        {
-            totalPrice: 2500,
-            policyPrice: 1100,
-            claimPrice: 1400
-        },
-        {
-            totalPrice: 2500,
-            policyPrice: 1100,
-            claimPrice: 1400
-        }
-        ];
+    {
+        totalPrice: 2500,
+        policyPrice: 1100,
+        claimPrice: 1400
+    },
+    {
+        totalPrice: 2500,
+        policyPrice: 1100,
+        claimPrice: 1400
+    }
+];
 
-    return (
-        <div className="main-inside">
-            <div className="reports">
-                <div className="cover">
-                    <img src={coverImg1} alt="" title=""
-                         className="cover-img-1"/>
-                    <img src={coverImg2} alt="" className="cover-img-2"/>
-                    <div className="cover-info">
-                        <div className="cover-title">
-                            <p>
-                                20% Safe On Your First Account
-                            </p>
-                        </div>
-                        <div className="flex-container align-middle align-center code-wrapper">
-                            <div className="code">
-                                NEWBIE20
+class Reports extends React.Component {
+    state = {
+        value: 'NEWBIE20',
+        copied: false,
+    };
+    render() {
+        return (
+            <div className="main-inside">
+                <div className="reports">
+                    <div className="cover">
+                        <img src={coverImg1} alt="" title=""
+                             className="cover-img-1"/>
+                        <img src={coverImg2} alt="" className="cover-img-2"/>
+                        <div className="cover-info">
+                            <div className="cover-title">
+                                <p>
+                                    20% Safe On Your First Account
+                                </p>
                             </div>
-                            <div className="copy-code">
-                                Copy Code
+                            <div className="flex-container align-middle align-center code-wrapper">
+                                <div className="code">
+                                    {this.state.value}
+                                </div>
+                                <div className="copy-code">
+                                    <CopyToClipboard text={this.state.value}
+                                                     onCopy={() => this.setState({copied: true})}>
+                                        <div>
+                                            Copy Code
+                                        </div>
+                                    </CopyToClipboard>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="card-list">
-                    {cardList.map((item, index)=>{
-                        return (
-                            <Card totalPrice={item.totalPrice} claimPrice={item.claimPrice} policyPrice={item.policyPrice} key={index}/>
-                        )
-                    })}
+                    <div className="card-list">
+                        {cardList.map((item, index)=>{
+                            return (
+                                <Card totalPrice={item.totalPrice} claimPrice={item.claimPrice} policyPrice={item.policyPrice} key={index}/>
+                            )
+                        })}
 
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Reports;
